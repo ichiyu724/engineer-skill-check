ac09cd21ae814cc657e6c9887c6635594888a1b4
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-11-06 21:37:14 +0900

    employeeのmodel_specにメールアドレスが入力できているときにバリデーションが通ることのテストを記述

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "社員新規追加" do
+    context "メールアドレス入力欄が正常な時" do
+      it "メールアドレスが入力できている" do
+        expect(employee).to be_valid
+      end
+    end
+  end
 end
