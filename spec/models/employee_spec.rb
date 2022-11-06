05875c35ccc512b04557aa0b4d72d524cc7e4b00
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-11-07 00:02:01 +0900

    氏名（名）が空欄の時にエラーになることのテストを追加

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -34,6 +34,12 @@ RSpec.describe Employee, type: :model do
         employee.valid?
         expect(employee.errors.full_messages).to include("氏名（姓） が入力されていません")
       end
+
+      it "氏名（名）が空欄だと登録できない" do
+        employee.first_name = ""
+        employee.valid?
+        expect(employee.errors.full_messages).to include("氏名（名） が入力されていません")
+      end
     end
   end
 end
