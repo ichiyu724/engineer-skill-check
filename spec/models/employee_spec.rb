cbfcab86a7d71f43baafffa415819f61861b416b
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-11-06 22:48:56 +0900

    入社年月日が空欄だった時にエラーが出ることのテストを記述

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -4,17 +4,23 @@ RSpec.describe Employee, type: :model do
   let(:employee) { create(:employee) }
 
   describe "社員新規追加" do
-    context "メールアドレス入力欄が正常な時" do
-      it "メールアドレスが入力できている" do
+    context "新規登録が成功するとき" do
+      it "必須項目が全て入力できている" do
         expect(employee).to be_valid
       end
     end
 
-    context "メールアドレス入力欄が不正の時" do
+    context "新規登録が失敗するとき" do
       it "emailが空欄だと登録できない" do
         employee.email = ""
         employee.valid?
         expect(employee.errors.full_messages).to include("メールアドレス が入力されていません")
+      end
+
+      it "入社年月日が空欄だと登録できない" do
+        employee.date_of_joining = ""
+        employee.valid?
+        expect(employee.errors.full_messages).to include("入社年月日 が入力されていません")
       end
     end
   end
