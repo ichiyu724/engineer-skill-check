3962170454dd895a8fa7468c5c34f0a2f201eae1
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-11-07 21:05:46 +0900

    posts_controllerにindex,new,create,edit,show,updateのアクションを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def show
+
+    end
+
+    def update
+        
+    end
 end
