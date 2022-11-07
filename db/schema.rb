5c807a7a2afac284f8aced74fcb9f479bb4a65a2
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-11-07 23:38:25 +0900

    empoyeeとのアソシエーションエラーが起きたため、authorカラムを削除し、employee_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_07_125315) do
+ActiveRecord::Schema.define(version: 2022_11_07_143603) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
-    t.integer "author", null: false
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "employee_id", null: false
+    t.index ["employee_id"], name: "index_articles_on_employee_id"
   end
 
   create_table "departments", force: :cascade do |t|
@@ -63,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_11_07_125315) do
     t.index ["employee_id"], name: "index_profiles_on_employee_id"
   end
 
+  add_foreign_key "articles", "employees"
   add_foreign_key "employees", "departments"
   add_foreign_key "employees", "offices"
   add_foreign_key "profiles", "employees"
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article) { create(:article) }
+
+  describe "お知らせ新規投稿" do
+    context "新規投稿が成功するとき" do
+      it "必須項目が全て入力できている" do
+        expect(article).to be_valid
+      end
+    end
+  end
 end
