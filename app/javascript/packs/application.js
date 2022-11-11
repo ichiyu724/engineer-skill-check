584c8530573bf8682e3ad38accc9f38aaa875ec5
ichiyu724 <yutaro_ichioka@ichiokayuutarounoMacBook-Pro.local>
2022-11-12 00:33:37 +0900

    お知らせ一覧にページネーションを実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ import Rails from "@rails/ujs";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 
-import "jquery";
 import "bootstrap";
 import "../stylesheets/application.scss";
+import "jquery";
 
 Rails.start();
 ActiveStorage.start();
